404d16b39488ef1652caad4c6b086a9d74f67afc
Folonder <74664782+Folonder@users.noreply.github.com>
2023-01-19 15:32:54 +0300

    release v1.0

--- server/public/js/script.js
+++ server/public/js/script.js
@@ -120,19 +120,16 @@ document.onscroll = function(){
  form.onsubmit = function(event){
          var xhr = new XMLHttpRequest();
          var formData = new FormData(form);
-         //open the request
-         xhr.open('POST','http://localhost:3000/api/person')
+         xhr.open('POST','https://migratemaster.onrender.com/api/person')
          xhr.setRequestHeader("Content-Type", "application/json");
- 
-         //send the form data
          xhr.send(JSON.stringify(Object.fromEntries(formData)));
  
          xhr.onreadystatechange = function() {
              if (xhr.readyState == XMLHttpRequest.DONE) {
-                 form.reset(); //reset form after AJAX success or do something else
+                 form.reset();
              }
          }
-         //Fail the onsubmit to avoid page refresh.
+         alert("Вы успешно зарегистрировались")
          return false; 
      }
 

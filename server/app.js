82a7db0b959631af18a695030ca08d3ba595f676
Folonder <74664782+Folonder@users.noreply.github.com>
2023-01-19 13:03:50 +0300

    add html to server

--- server/app.js
+++ server/app.js
@@ -6,15 +6,15 @@ const CompanyModel = require('./database/models/company')
 const PersonModel = require('./database/models/person')
 const personRoutes = require('./routes/personRoutes.js')
 const companyRoutes = require('./routes/companyRoutes.js')
-const port  = process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 
 function start(){
     try {
         db.authenticate();
         db.sync();
-        console.log('Connection has been established successfully.');
         startApp()
+        console.log('Connection has been established successfully.');
     } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
@@ -25,8 +25,14 @@ function startApp(){
     app.use(express.json())
     app.use('/api', personRoutes)
     app.use('/api', companyRoutes)
+
+    app.use(express.static('public'))
+    app.use('/css', express.static(__dirname + 'public/css'))
+    app.use('/js', express.static(__dirname + 'public/js'))
+    app.use('/logo', express.static(__dirname + 'public/logo'))
+    
     app.get('/', function(req, res){
-        res.send('Hello world')
+        res.sendFile(__dirname + '/view/index.html')
     })
     app.listen(port, function(){
         console.log(`Server started at ${port}`)
